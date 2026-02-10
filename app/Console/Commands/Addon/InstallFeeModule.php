b10b71a257d767d385a9503e77e9f97554a559de
Devteam <devteam@gegok12.com>
2026-02-10 19:14:20 +0530

    fix fees command

--- app/Console/Commands/Addon/InstallFeeModule.php
+++ app/Console/Commands/Addon/InstallFeeModule.php
@@ -124,7 +124,7 @@ class InstallFeeModule extends Command
 
             // Step 6: Add to app.js
             $appJsPath = resource_path('assets/js/app.js');
-            if (!str_contains(file_get_contents($appJsPath), "require('./gfee')")) {
+            if (!str_contains(file_get_contents($appJsPath), "require('./gfees')")) {
                 file_put_contents($appJsPath, file_get_contents($appJsPath) . "\nrequire('./gfee');\n");
                 $this->info("Updated app.js");
             }
