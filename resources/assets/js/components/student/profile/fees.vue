fbf1bc062350daccd8dbe5ef6390f926abdd9517
Devteam <devteam@gegok12.com>
2026-03-05 16:55:50 +0530

    update student profile

--- resources/assets/js/components/student/profile/fees.vue
+++ resources/assets/js/components/student/profile/fees.vue
@@ -54,7 +54,7 @@
                         <th class="text-left text-sm px-2 py-2 text-grey-darker">Amount</th>
                         <th class="text-left text-sm px-2 py-2 text-grey-darker">Paid On</th>
                         <th class="text-left text-sm px-2 py-2 text-grey-darker">Notify Parents</th>
-                        <th v-if="mode === 'admin'" class="text-left text-sm px-2 py-2 text-grey-darker">Actions</th>
+                        <th class="text-left text-sm px-2 py-2 text-grey-darker">Actions</th>
                     </tr>
                 </thead>   
                 <tbody v-for="fee in fees">
@@ -67,7 +67,7 @@
                         <td class="py-3 px-2"><p class="font-semibold text-xs">{{ fee.amount }}</p></td>
                         <td class="py-3 px-2"><p class="font-semibold text-xs">{{ fee.paid_on }}</p></td>
                         <td class="py-3 px-2"><p class="font-semibold text-xs">{{ fee.notify_parent }}</p></td>
-                        <td v-if="mode === 'admin'" class="py-3 px-2">
+                        <td class="py-3 px-2">
                             <div class="flex items-center">
                                 <a href="#" class="font-semibold text-xs" @click="showEdit(fee.id)" title="Edit" v-if="fee.feePayment_id != null && fee.status == 0">
                                     <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 477.873 477.873" xml:space="preserve" class="w-4 h-4 fill-current mx-1"><g><g><path d="M392.533,238.937c-9.426,0-17.067,7.641-17.067,17.067V426.67c0,9.426-7.641,17.067-17.067,17.067H51.2 c-9.426,0-17.067-7.641-17.067-17.067V85.337c0-9.426,7.641-17.067,17.067-17.067H256c9.426,0,17.067-7.641,17.067-17.067 S265.426,34.137,256,34.137H51.2C22.923,34.137,0,57.06,0,85.337V426.67c0,28.277,22.923,51.2,51.2,51.2h307.2 c28.277,0,51.2-22.923,51.2-51.2V256.003C409.6,246.578,401.959,238.937,392.533,238.937z"></path></g></g> <g><g><path d="M458.742,19.142c-12.254-12.256-28.875-19.14-46.206-19.138c-17.341-0.05-33.979,6.846-46.199,19.149L141.534,243.937 c-1.865,1.879-3.272,4.163-4.113,6.673l-34.133,102.4c-2.979,8.943,1.856,18.607,10.799,21.585 1.735,0.578,3.552,0.873,5.38,0.875c1.832-0.003,3.653-0.297,5.393-0.87l102.4-34.133c2.515-0.84,4.8-2.254,6.673-4.13 l224.802-224.802C484.25,86.023,484.253,44.657,458.742,19.142z M434.603,87.419L212.736,309.286l-66.287,22.135l22.067-66.202 L390.468,43.353c12.202-12.178,31.967-12.158,44.145,0.044c5.817,5.829,9.095,13.72,9.12,21.955 C443.754,73.631,440.467,81.575,434.603,87.419z"></path></g></g></svg>
